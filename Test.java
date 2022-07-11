c3dfe5788c8d58cc72f03564f290ffb6c78dce36
khushbu22193 <109040350+khushbu22193@users.noreply.github.com>
2022-07-11 22:50:02 +0100

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package codebustertesting;
+
+public class Test {
+    public static void main(String [] args){
+        System.out.println("Codebuster Testing"); //printing statement
+        System.out.println("khushbu");
+        System.out.println("yug");
+        System.out.print("parekh");
+        System.out.println("yugdev");
+        System.out.print("kyp");
+        /*system: is final class.
+        out:is a static member of the system class
+        println or print: is a method.*/
+    }
+}
